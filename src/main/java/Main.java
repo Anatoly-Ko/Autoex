0b77482664abf961533b48bc5ea74746d31c5972
Анатолий Коропченко <korop4enko.tolik@yandex.ru>
2022-05-26 22:32:57 +0300

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args){
+        Polin numbers = new Polin();
+        int sum = numbers.pol(5);
+        System.out.println("Sum from 0 to 5 = " + sum);
+    }
+}
---
+++ src/main/java/Polin.java
@@ -0,0 +1,9 @@
+public class Polin {
+    public int pol(int number){
+        int result=0;
+        for(int i=number;i>=0;i--){
+            result+=i ;
+        }
+        return result;
+    }
+}
